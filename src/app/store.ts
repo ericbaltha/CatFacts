54dad428b66022859f08631a176781562488a5bd
ebalthazard <ebalthazard@deepsight.ca>
2023-01-27 14:46:41 -0500

    Good luck

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import catFactsSlice from '../features/CatFacts/catFactsSlice';
+
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: { },
 });
 
 export type AppDispatch = typeof store.dispatch;
